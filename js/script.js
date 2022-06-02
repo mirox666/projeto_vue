77a82cca7e5e23b721c20c5442822e19e0f66475
Matheus Raposo <123lampiao10@gmail.com>
2022-06-02 20:26:53 -0300

    Tornando o menu funcional

--- js/script.js
+++ js/script.js
@@ -7,12 +7,30 @@ const app = Vue.createApp({
                 cambio:false,
                 diaSemana:false,
                 desconto:false
-            }
+            },
+            mensagem: {
+                'idade' : "Cálculo da idade a partir do ano de nascimento inserido pelo usuário",
+                'cambio': "Cálculo do valor em reais a partir de um valor em dólar e cotação inserida pelo usuário",
+                'diaSemana' : "Exibir o dia da semana a partir de uma data inserida pelo usuário",
+                'desconto' : "Calcular  o  resultado  a  partir  de  um  valor  base  e  o percentual de desconto indicados pelo usuário"
+            },
+            textoInicial:'Olá, Seja Bem Vindo ao Nosso Sistema'
         }
     },
     methods:{
         verificarLink(classe){
-        console.log(classe)
+        //console.log(classe[1])
+        let itens = classe[1]// Um parâmetro que irá conter um vetor com as classes da tag na qual a função está sendo chamada.
+        //for..in permite percorrer um objeto    
+        for(elementos in this.bloco)
+        //console.log(elementos)
+        if(itens == elementos){
+            this.bloco[elementos] = true
+            this.textoInicial = this.mensagem[elementos]
+        }
+        else{
+            this.bloco[elementos] = false
+        }
         }
     }
 })
